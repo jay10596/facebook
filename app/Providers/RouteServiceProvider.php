69ee95533912c863cc2881726912d6cbccb305ed
Jay10596 <jay.modi105@gmail.com>
2020-05-25 12:01:35 +1000

    Frontend setup

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,13 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
     protected $namespace = 'App\Http\Controllers';
 
     /**
@@ -21,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-red-500">
+        <h1>Hello</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+    }
+</script>
+
+<style>
+
+</style>
